9a42cee49353ca9ca5bf5b4c29fa0cc6df24136d
Om Sharma <omsharma050322@gmail.com>
2023-09-04 23:46:37 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        // <h1>hello</h1>
+        <>
+            <nav className='navbar'>
+                <div className="navbar-container">
+                    <h1>hello</h1>
+
+                </div>
+            </nav>
+        </>
+    );
+
+
+
+
+}
+
+export default Navbar;
