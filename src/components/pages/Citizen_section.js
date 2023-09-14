ed84f24ff635488331a2646690ba7e8276053437
Om Sharma <omsharma050322@gmail.com>
2023-09-14 23:30:49 +0530

    Citizen-section added

--- src/components/pages/Citizen_section.js
+++ src/components/pages/Citizen_section.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Citizen_section.css';
 
 // import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-// import sidebar_menu_citizen from './sidebar_menu_citizen';
+import sidebar_menu_citizen from './sidebar_menu_citizen';
+import './Citizen_section.css'
 
 
 function Citizen_section() {
     return (
-        <>
-            <section className='citizen-sidebar'>
+        <div >
 
-                <Link>File New Case</Link>
-                <Link>File New Case</Link>
-                <Link>File New Case</Link>
-                <Link>File New Case</Link>
+            <section className='citizen-main-body'>
+                <img src="images/8777961.jpg" alt="" className='citizen-sec-image' />
+                <div className="citizen-nav">
+                    <h1 className='citizen-question'>What would you like to do ?</h1>
+                    <div className="citizen-btn-nav">
+                        <div>
+                            <button className="citizen-nav-button">
+                                File a New Case
+                            </button>
+                            <button className="citizen-nav-button">Upcoming Hearing</button>
+                        </div>
+                        <div>
+                            <button className="citizen-nav-button">Past hearing</button>
+                            <button className="citizen-nav-button">Upload Evidence</button>
+                        </div>
+
+                    </div>
+
+
+                </div>
             </section>
-        </>
+        </div>
     )
 }
 
