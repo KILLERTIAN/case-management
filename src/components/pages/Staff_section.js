258931e4984302f590273ad54894bddf09b0e05a
Om Sharma <omsharma050322@gmail.com>
2023-09-15 23:33:36 +0530

    Staff section js

---
+++ src/components/pages/Staff_section.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import sidebar_menu_staff from './sidebar_menu_staff';
+import './Staff_section.css'
+
+
+function Staff_section() {
+    return (
+        <div >
+            <section className='staff-main-body'>
+                <img src="images/lawyer.avif" alt="" className='staff-sec-image' />
+                <div className="staff-nav">
+                    <h1 className='staff-question'>What would you like to do ?</h1>
+                    <div className="staff-btn-nav">
+                        <div>
+                            <button className="staff-nav-button">
+                                Add New Record
+                            </button>
+                            <button className="staff-nav-button">Schedule Hearing</button>
+                        </div>
+                        <div>
+                            <button className="staff-nav-button">Upcoming hearing</button>
+                            <button className="staff-nav-button">Evidence</button>
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Staff_section;
